40ee7d8d0d64a4c1d91550f229a6f30e35df46f3
Toh Yue Zhen <smithquaz@gmail.com>
2022-10-26 20:00:47 +0800

    some changes

--- time_test/merge_sort.py
+++ time_test/merge_sort.py
@@ -1,34 +1,34 @@
-def merge_sort(unsorted_array):
-    if len(unsorted_array) > 1:
-        mid = len(unsorted_array) // 2  # Finding the mid of the array
-        left = unsorted_array[:mid]  # Dividing the array elements
-        right = unsorted_array[mid:]  # into 2 halves
+# def merge_sort(unsorted_array):
+#     if len(unsorted_array) > 1:
+#         mid = len(unsorted_array) // 2  # Finding the mid of the array
+#         left = unsorted_array[:mid]  # Dividing the array elements
+#         right = unsorted_array[mid:]  # into 2 halves
 
-        merge_sort(left)
-        merge_sort(right)
+#         merge_sort(left)
+#         merge_sort(right)
 
-        i = j = k = 0
+#         i = j = k = 0
 
-        #  data to temp arrays L[] and R[]
-        while i < len(left) and j < len(right):
-            if left[i] < right[j]:
-                unsorted_array[k] = left[i]
-                i += 1
-            else:
-                unsorted_array[k] = right[j]
-                j += 1
-            k += 1
+#         #  data to temp arrays L[] and R[]
+#         while i < len(left) and j < len(right):
+#             if left[i] < right[j]:
+#                 unsorted_array[k] = left[i]
+#                 i += 1
+#             else:
+#                 unsorted_array[k] = right[j]
+#                 j += 1
+#             k += 1
 
-        # Checking if any element was left
-        while i < len(left):
-            unsorted_array[k] = left[i]
-            i += 1
-            k += 1
+#         # Checking if any element was left
+#         while i < len(left):
+#             unsorted_array[k] = left[i]
+#             i += 1
+#             k += 1
 
-        while j < len(right):
-            unsorted_array[k] = right[j]
-            j += 1
-            k += 1
+#         while j < len(right):
+#             unsorted_array[k] = right[j]
+#             j += 1
+#             k += 1
 
 
 # Code to print the list
